5d89853ba1dad4eb3cd7a751e0c2bc7a9fc820b7
Manuel Eusebio de Paz Carmona <Pelirrojo@users.noreply.github.com>
2017-07-24 22:50:58 +0200

    Adding an event bus through Redis Pub/Sub

--- node-master/index.js
+++ node-master/index.js
@@ -2,8 +2,6 @@ var express = require('express')
 var http = require('http')
 var redis = require('redis')
 
-var whoami = 'microservice-' + parseInt(Math.random()*1000000000)
-
 // PUB_SUB BUS //////////////////////////////////////////////////////////////////////
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT)
 
@@ -16,26 +14,11 @@ console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
 const client = redis.createClient('6379', 'redis')
 
-// Self register
+setInterval(function () {
+  var sample = { id: Math.random(), date: new Date().toISOString() }
+  client.publish("bus", JSON.stringify(sample))
+}, 3000)
 
-
-client.on("message", function (channel, message) {
-
-  // var data = JSON.parse(message)
-  // TODO: It's my turn?
-  console.log("Message '" + message + "' on channel '" + channel + "' arrived!")
-})
-
-client.subscribe("bus")
-
-/*
-
-// TODO investigate an alternative to implement RoundRobin of messages from bus
-client.publish("system", JSON.stringify({code: "HELLO", id: whoami }))
-process.on('exit', function () {
-  client.publish("system", JSON.stringify({code: "BYE", id: whoami }))
-});
-*/
 // REST SERVICE /////////////////////////////////////////////////////////////////////
 
 const app = express()
